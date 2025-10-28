a8f41c15b704802fe0c561538f3f6b5b049b7f27
Syifa Aryanta <syifaar21@gmail.com>
2025-10-29 00:17:53 +0700

    Tutorial
    Understanding JSX in ReactJS and ES6 (2)

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UserCard from "./UserCard";
+import { getUsers } from "./Service";
 
 function Exercise() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getUsers();
+        setUsers(data);
+      } catch (error) {
+        console.error("[Component] Gagal menampilkan data:", error.message);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <div className="min-h-screen bg-gray-100 p-6">
@@ -9,21 +23,9 @@ function Exercise() {
           User Cards
         </h1>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
-          <UserCard 
-            name="April"
-            email="april@gmail.com"
-            city="Semarang"
-          />
-          <UserCard 
-            name="Yani"
-            email="yani@gmail.com"
-            city="Ungaran"
-          />
-          <UserCard 
-            name="Safitri"
-            email="safitri@gmail.com"
-            city="Kendal"
-          />
+          {users.map((user) => (
+            <UserCard key={user.email} {...user} />
+          ))}
         </div>
       </div>
     </>
